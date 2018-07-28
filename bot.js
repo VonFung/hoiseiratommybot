6414b3672ecc22c71d3365fb047491b2f0b16a73
VonFung <41778370+VonFung@users.noreply.github.com>
2018-07-29 04:44:05 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -43,7 +43,7 @@ var func_help = {
             for(i=1; i<func.length; i++) {
                 msg = msg + "\n" + func[i].CODE + "\t\t\t" + func[i].DESCRIPTION;
             }
-            message.channel.sendMessage(msg);
+            message.channel.send(msg);
         } else {
             var j;
             for(j=0; j<func.length; j++) {
@@ -222,102 +222,6 @@ client.on('message', message => {
     }
     
     message.reply('Invalid command ($help to view commands)');
-    
-    /*switch(token[0].toUpperCase()) {
-        case 'READY':    //For testing bot online
-            message.reply('YES!');
-            break;
-        case 'ADDMUSIC':
-            if(token.length < 4) {
-                message.reply("Wrong Format\n{$ADDMUSIC | music_code | URL | isYoutube?(bool:T/TRUE/F/FALSE) | default_volume(float,OPTIONAL)}");
-                return;
-            }
-            
-            var con = mysql.createConnection({
-                host: db_host,
-                user: db_user,
-                password: db_password,
-                database: db_schema
-            });
-                
-            con.connect(function(err) {
-                if(err) throw err;
-                var sql = "INSERT INTO playlist (CODE, URL, IS_YOUTUBE" + (token.length > 4?", " + token[4]:"") + ") VALUES ('"
-                          + token[1].toUpperCase() + "', '" + token[2] + "', " + ((token[3].toUpperCase() === "T" || token[3].toUpperCase() === "TRUE")?"TRUE":"FALSE")
-                           + (token.length > 4?", " + token[4]:"") + ")";
-                
-                console.log(sql);
-                con.query(sql, function(err, result) {
-                    if(err) throw err;
-                    message.reply('ADDED SUCCESSFULLY');
-                });
-            });
-            break;
-        case 'PLAY':    //play music
-            if(token.length < 2) {
-                message.reply('Invalid command\n{$PLAY _MUSIC_NAME}');   
-                return;
-            }
-       
-            
-            var con = mysql.createConnection({
-                host: db_host,
-                user: db_user,
-                password: db_password,
-                database: db_schema
-            });
-            
-            con.connect(function(err) {
-                if(err) throw err;
-                console.log("Connected!");
-                con.query("SELECT URL, IS_YOUTUBE, DEFAULT_VOLUME FROM playlist WHERE CODE = '" + token[1].toUpperCase() + "'", function (err, result, field) {
-                    if(err) throw err;
-                    if(result.length === 0) {
-                        message.reply("No such music");
-                        return;   
-                    } else {
-                        
-                        var isYoutubeOrNot = result[0].IS_YOUTUBE;
-                        var volume = result[0].DEFAULT_VOLUME;
-                        var url = result[0].URL;
-                        
-                        voiceChannel = message.member.voiceChannel;
-                        if(isYoutubeOrNot) {
-                            voiceChannel.join().then(connection => {
-                                console.log("joined channel");
-                                stream = ytdl(url, {filter : 'audioonly'});
-                                dispatcher = connection.playStream(stream);
-                                dispatcher.setVolume(volume);
-                                dispatcher.on("end", end => {
-                                    console.log("left channel");
-                                    voiceChannel.leave();
-                                });
-                            }).catch(err => console.log(err));
-                        } else {
-                            voiceChannel.join().then(connection => {
-                                console.log("joined channel");
-                                dispatcher = connection.playArbitraryInput(url);
-                                dispatcher.setVolume(volume);
-                                dispatcher.on("end", end => {
-                                    console.log("left channel");
-                                    voiceChannel.leave();
-                                });
-                            }).catch(err => console.log(err));
-                        }
-                        
-                    }
-                });
-            });
-            
-            break;
-        case 'STOP':    //stop music
-            console.log("left channel");
-            voiceChannel.leave();
-            break;
-        default:
-            message.reply('Invalid command');
-            return;
-    }*/
     
 });
 
