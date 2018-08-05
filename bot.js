7ff20b67d56352c43e8252bedba650db505c0429
VonFung <41778370+VonFung@users.noreply.github.com>
2018-08-05 22:54:56 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,7 +24,7 @@ var master_volume = 1;
 var music_queue = [];
 var now_playing_music = null;
 
-const update_time = new Date().getTime();
+const update_time = new Date().toLocaleTimeString();
 
 
 //---Objects for functions---
