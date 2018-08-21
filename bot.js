67f6b3020ba00491d0b6d3e2f02e84cea9b9c658
VonFung <41778370+VonFung@users.noreply.github.com>
2018-08-21 17:33:02 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -772,6 +772,7 @@ var func_test = {
         });*/
         nico.users.login.post({mail_tel: process.env.NICO_USERNAME, password: process.env.NICO_PW})
           .then(session => {
+            console.log("session = " + session);
             nico.video.flv.get({id: 'sm30939147', user_session: session})
               .then(result => {
                     console.log("result = " + result);
