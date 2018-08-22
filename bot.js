ae3aad10a8b37f5f7fa35fb9a4cef0eb661f7766
VonFung <41778370+VonFung@users.noreply.github.com>
2018-08-22 13:39:45 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -754,12 +754,12 @@ var func_sql = {
               sql = sql + " " + token[i];
           }
           ExecuteSQL(sql).then((result) => {
-              result_str = "";
+              /*result_str = "";
               for(let a=0; a<result.length; a++) {
                   result_str = result_str + decodeURIComponent(JSON.stringify(result[i])); 
               }
-              message.channel.send(result_str);
-              //message.channel.send(decodeURIComponent(JSON.stringify(result)));
+              message.channel.send(result_str);*/
+              message.channel.send(JSON.stringify(result).replace(/%22/g, '"'));
           }).catch((err) => {
               message.reply("Something error! Please refer to the log on Heroku");
               console.log(err);
