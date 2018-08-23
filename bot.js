a76e3042e6f2ce67c3f45ef8521f6419a2d1d017
VonFung <41778370+VonFung@users.noreply.github.com>
2018-08-23 20:32:06 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,10 +7,15 @@ const hook = new Webhook(process.env.WEBHOOK_URL);
 
 const client = new Discord.Client();
 
-const db_host = "den1.mysql1.gear.host"; //gearhost mysql server
+/*const db_host = "den1.mysql1.gear.host"; //gearhost mysql server
 const db_user = "hoiseiratommybot";
 const db_password = process.env.DB_PW;
-const db_schema = "hoiseiratommybot";
+const db_schema = "hoiseiratommybot";*/
+
+const db_host = process.env.DB_HOST;
+const db_user = process.env.DB_USER;
+const db_password = process.env.DB_PW;
+const db_dbname = proceses.env.DB_DBNAME;
 
 
 var voiceChannel;          //===================
@@ -1181,7 +1186,7 @@ function ExecuteSQL(sql) {
         host: db_host,
         user: db_user,
         password: db_password,
-        database: db_schema
+        dbname: db_dbname
     });
  
     return new Promise((resolve, reject) => {
