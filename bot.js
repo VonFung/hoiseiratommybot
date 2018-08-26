45342da2f692a3cbd9fd9a3dbbebc265e0279d72
VonFung <41778370+VonFung@users.noreply.github.com>
2018-08-27 01:44:46 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -766,7 +766,7 @@ var func_updateship = {
                 message.reply("Something error! Please refer to the log on Heroku");
                 console.log(err);
               });
-            }).catch(err) => {
+            }).catch((err) => {
               message.reply("Something error! Please refer to the log on Heroku");
               console.log(err);
             }
