652f1c7d7ec8341a06a412f16f51aaf4b89734e2
VonFung <41778370+VonFung@users.noreply.github.com>
2018-08-27 02:04:10 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -729,7 +729,7 @@ var func_updateship = {
               }
               var i;
               for(i=0; i<shipdata2.length; i++) {
-                  if(shipdata2.max_eq === null) {
+                  if(shipdata2[i].max_eq === null) {
                     continue; 
                   }
                   var j;
