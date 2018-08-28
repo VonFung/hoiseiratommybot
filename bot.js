e4e4322e4adb2dd3d0271fc7462c615d26a69c74
VonFung <41778370+VonFung@users.noreply.github.com>
2018-08-28 10:40:26 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js');
 const ytdl = require('ytdl-core');  //For music streaming
 const Webhook = require('webhook-discord');
 var mysql = require('mysql');
-var fs = require('fs');
 
 const hook = new Webhook(process.env.WEBHOOK_URL);
 
@@ -1421,7 +1420,6 @@ client.on('ready', () => {
     /*client.guilds.forEach((id, guild) => {
         console.log("Guild id: " + id + "/" + guild.id);
     });*/
-    console.log("path exists = " + fs.existsSync("./db/ships.nedb"));
 });
 
 client.on('message', message => {
