43e9ccc309522f937f240f22ae7cefa3a36b0d08
VonFung <41778370+VonFung@users.noreply.github.com>
2018-08-28 12:32:53 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1278,6 +1278,7 @@ var func_updateship = {
       
         httpRequest("http://api.kcwiki.moe/ships").then((res) => {
               var shipdata = JSON.parse("[" + res.replace(/\n/g, ",") + "]");
+              console.log("shipdata = " + JSON.stringify(shipdata));
               var i;
               let sql = "REPLACE INTO Ship (id, ja_jp, ja_kana, ja_romaji, zh_tw, asw, asw_max, los, los_max, speed"
                         +", fuel_consum, ammo_consum, slot, type, _id) VALUES ? ";
