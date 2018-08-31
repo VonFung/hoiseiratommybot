d5dfc5d5dd8502629801dcea88439a09c1d5f545
VonFung <41778370+VonFung@users.noreply.github.com>
2018-08-31 14:07:53 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1206,10 +1206,10 @@ var func_searchfleet = {
                                                 }
                                                 embed_msg.embed.fields.push(ship);
                                             }
-                                            embed_msg.embed.fields.push(...{name: "索敵(33式):", values: (los_ship + los_item - 48 + 2 * (6 - embed_msg.fields.length)).toFixed(2) + "(n=1)/"
-                                                             +(los_ship + 3 * los_item - 48 + 2 * (6 - embed_msg.fields.length)).toFixed(2) + "(n=3)/"
-                                                             +(los_ship + 4 * los_item - 48 + 2 * (6 - embed_msg.fields.length)).toFixed(2) + "(n=4)"});
-                                            embed_msg.embed.field.push(...{name: "制空:", values: Math.floor(aa) + "(" + Math.floor(min_aa) + "~" + Math.floor(max_aa) + ")"});
+                                            embed_msg.embed.fields.push({name: "索敵(33式):", values: (los_ship + los_item - 48 + 2 * (6 - embed_msg.embed.fields.length)).toFixed(2) + "(n=1)/"
+                                                             +(los_ship + 3 * los_item - 48 + 2 * (6 - embed_msg.embed.fields.length)).toFixed(2) + "(n=3)/"
+                                                             +(los_ship + 4 * los_item - 48 + 2 * (6 - embed_msg.embed.fields.length)).toFixed(2) + "(n=4)"});
+                                            embed_msg.embed.field.push({name: "制空:", values: Math.floor(aa) + "(" + Math.floor(min_aa) + "~" + Math.floor(max_aa) + ")"});
                                             this.MESSAGE.delete();
                                             msg.channel.send(embed_msg);
                                             msg.delete();
