2c931f66140e39a1a501241a79285da5e474d475
VonFung <41778370+VonFung@users.noreply.github.com>
2018-09-10 22:15:50 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -2343,7 +2343,7 @@ function DisplayFleet(fleet) {
                              +(los_ship + 3 * los_item - 48 + 2 * (6 - no_of_ship)).toFixed(2) + "(n=3)/"
                              +(los_ship + 4 * los_item - 48 + 2 * (6 - no_of_ship)).toFixed(2) + "(n=4)"});
             embed_msg.embed.fields.push({name: "制空:", value: aa + "+(" + aa + "~" + max_aa + ")"});
-            if(!(fleet.remark === undefined || fleet.remark === null || )) {
+            if(!(fleet.remark === undefined || fleet.remark === null || fleet.remark === "")) {
                 embed_msg.embed.fields.push({name: "Remark:", value: fleet.remark});
             }
             resolve(embed_msg);
